f9add16f2636319e38ebbdeccade341f75a8a6a5
vikas <257127716+vikasshuklatrainer@users.noreply.github.com>
2026-01-28 20:20:24 +0530

    Update updateStudentLambda.py

--- updateStudentLambda.py
+++ updateStudentLambda.py
@@ -5,7 +5,6 @@ from decimal import Decimal
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('Students')
 
-# Convert Decimal → int/float
 def decimal_to_native(obj):
     if isinstance(obj, Decimal):
         return int(obj) if obj % 1 == 0 else float(obj)
@@ -16,13 +15,17 @@ def lambda_handler(event, context):
 
     body = None
 
-    # Case 1: Body as string
+    # Case 1: From Function URL / API Gateway
     if isinstance(event.get("body"), str):
         body = json.loads(event["body"])
 
-    # Case 2: Body already dict
+    # Case 2: From direct invocation
     elif isinstance(event.get("body"), dict):
         body = event["body"]
+
+    # Case 3: Console plain JSON
+    elif isinstance(event, dict):
+        body = event
 
     if not body:
         return {
@@ -30,7 +33,6 @@ def lambda_handler(event, context):
             "body": json.dumps({"message": "Request body is required"})
         }
 
-    # Validate input
     required_fields = ["studentId", "name", "age", "course"]
     for field in required_fields:
         if field not in body:
@@ -42,9 +44,7 @@ def lambda_handler(event, context):
     response = table.update_item(
         Key={"studentId": body["studentId"]},
         UpdateExpression="SET #n = :name, age = :age, course = :course",
-        ExpressionAttributeNames={
-            "#n": "name"
-        },
+        ExpressionAttributeNames={"#n": "name"},
         ExpressionAttributeValues={
             ":name": body["name"],
             ":age": body["age"],
